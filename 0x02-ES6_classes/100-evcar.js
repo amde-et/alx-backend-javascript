6fef992b7eda7ac7d42ae1996c891551f8a9b5a2
Amdebirhan Abebe <99601438+amde-et@users.noreply.github.com>
2022-12-23 11:03:41 +0300

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,15 @@
 import Car from './10-car';
 
-export default class EVCar extends Car {
+class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  // eslint-disable-next-line
   cloneCar() {
-    return new Car[Symbol.species]();
+    const Obj = super.constructor || super[Symbol.species];
+    return new Obj();
   }
 }
+
+export default EVCar;
